bd5dba8e3c7c6c347e6e4b5b9b0020bde5c78582
yashasvi <yashpant20@gmail.com>
2020-12-05 22:56:11 +0545

    1st commit

---
+++ src/main/java/com/elasticsearchperformanceanalyzer/esperformanceanalyzer/EsperformanceanalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.elasticsearchperformanceanalyzer.esperformanceanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsperformanceanalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsperformanceanalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elasticsearchperformanceanalyzer/esperformanceanalyzer/EsperformanceanalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elasticsearchperformanceanalyzer.esperformanceanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsperformanceanalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
