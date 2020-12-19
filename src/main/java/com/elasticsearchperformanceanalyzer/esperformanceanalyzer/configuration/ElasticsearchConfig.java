b2e470e3cee823701215a5f6e2263b49560acf39
yashasvi.pant <SLCSLC1a@>
2020-12-19 11:03:07 +0545

    Refine AWS Elasticsearch access

--- src/main/java/com/elasticsearchperformanceanalyzer/esperformanceanalyzer/configuration/ElasticsearchConfig.java
+++ src/main/java/com/elasticsearchperformanceanalyzer/esperformanceanalyzer/configuration/ElasticsearchConfig.java
@@ -1,7 +1,11 @@
 package com.elasticsearchperformanceanalyzer.esperformanceanalyzer.configuration;
 
 
+import com.amazonaws.auth.AWS4Signer;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import org.apache.http.HttpHost;
+import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
@@ -19,29 +23,39 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ElasticsearchConfig {
 
-    @Value("${elasticsearch.host}")
-    public String host;
+//    @Value("${elasticsearch.host}")
+//    public String host;
+//
+//    @Value("${elasticsearch.port}")
+//    public int port;
+//
+//    @Value("${elasticsearch.username}")
+//    public String username;
+//
+//    @Value("${elasticsearch.password}")
+//    public String password;
 
-    @Value("${elasticsearch.port}")
-    public int port;
+    //    @Value("${elasticsearch.password}")
+//    public String password;
 
-    @Value("${elasticsearch.username}")
-    public String username;
-
-    @Value("${elasticsearch.password}")
-    public String password;
+    private static final String serviceName = "es";
+    private static final String region = "ap-south-1";
+    private static final String aesEndpoint = "https://search-test-es-uslahdckphzjvrtjxnnepwfxyq.ap-south-1.es.amazonaws.com";
+    static final AWSCredentialsProvider credentialsProvider = new DefaultAWSCredentialsProviderChain();
 
     @Bean
     public RestHighLevelClient elasticsearchClient() {
 
-        return new RestHighLevelClient(
-                RestClient.builder(
-                        HttpHost.create("https://search-es-test-hfvxnaeq6toidr6qahbh25ojje.ap-south-1.es.amazonaws.com")));
+        AWS4Signer signer = new AWS4Signer();
+        signer.setServiceName(serviceName);
+        signer.setRegionName(region);
+        HttpRequestInterceptor interceptor = new AWSRequestSigningApacheInterceptor(serviceName, signer, credentialsProvider);
+        return new RestHighLevelClient(RestClient.builder(HttpHost.create(aesEndpoint)).setHttpClientConfigCallback(hacb -> hacb.addInterceptorLast(interceptor)));
     }
 
     @Bean
     public String clusterInfo() {
-        return  "https://search-es-test-hfvxnaeq6toidr6qahbh25ojje.ap-south-1.es.amazonaws.com/";
+        return  aesEndpoint;
     }
 
 
