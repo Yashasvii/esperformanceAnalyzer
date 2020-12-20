44540ad83028fb85b27d8171d8055bafb7f3ac57
yashasvi.pant <SLCSLC1a@>
2020-12-21 00:16:21 +0545

    Customize ES parameter

---
+++ src/main/java/com/elasticsearchperformanceanalyzer/esperformanceanalyzer/controller/SearchController.java
@@ -0,0 +1,30 @@
+package com.elasticsearchperformanceanalyzer.esperformanceanalyzer.controller;
+
+import com.elasticsearchperformanceanalyzer.esperformanceanalyzer.constants.PathConstants;
+import com.elasticsearchperformanceanalyzer.esperformanceanalyzer.request.ESCreationUpdateDeleteRequest;
+import com.elasticsearchperformanceanalyzer.esperformanceanalyzer.request.ESSearchRequest;
+import com.elasticsearchperformanceanalyzer.esperformanceanalyzer.services.SearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yashasvi
+ */
+
+@RestController
+@RequestMapping(PathConstants.SEARCH)
+public class SearchController {
+
+    @Autowired
+    SearchService searchService;
+
+    @PostMapping(path = PathConstants.SEARCH_QUERY)
+    public ResponseEntity<Object> searchQuery(@RequestBody ESSearchRequest esSearchRequest) {
+        return searchService.searchQuery(esSearchRequest);
+    }
+
+}
